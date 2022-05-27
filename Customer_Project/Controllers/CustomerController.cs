d6dfd9ab541f861fafe7709e4e5d7698392c9690
yesiltastubanur@gmail.com <yesiltastubanur@gmail.com>
2022-05-27 12:40:14 +0300

    GetByNation eklendi

--- Customer_Project/Controllers/CustomerController.cs
+++ Customer_Project/Controllers/CustomerController.cs
@@ -21,20 +21,24 @@ namespace Customer_Project.Controllers
         [HttpGet("GetAll")]
         public IActionResult GetAll()
         {
-            var Customer1 = _db.customer.ToList(); 
-          
-                                   
+            var Customer1 = _db.customer.ToList();
+
+
             return Ok(Customer1);
         }
 
         [HttpGet("GetById")] // ıdye göre customer değeri döner. ıd yoksa hata döner
         public IActionResult GetById(int id)
         {
-
             var customer = _db.customer.FirstOrDefault(x => x.Id == id);
-
             return Ok(customer);
+        }
 
+        [HttpGet("GetByNation")]
+        public IActionResult GetGirl(char nation)
+        {
+            var customer = _db.customer.Where(x => x.Nation == nation);
+            return Ok(customer);
         }
 
         [HttpPost("Create")]
